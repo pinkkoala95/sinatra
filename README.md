cdfe1706fd7a89a904c015184295eeb595dfad14
pinkkoala95 <emma.keeley@madetech.com>
2022-11-07 16:35:09 +0000

    initial addition of gov stylesheets

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ The remit of this task was to:
  - Write an automated test for the page (https://sinatrarb.com/testing.html)
  - Create a second page using TDD
  - Show a picture of a Llama on the page. 
+ - Use the Gov design system (how do you import it? they've got docs!) to style your pages like a gov website
+ (https://design-system.service.gov.uk/get-started/production/)
+
 
 ![Llama](https://birdsofpreycentre.co.uk/wp-content/uploads/2021/05/PHOTO-2021-05-04-14-10-14-1.jpg)
 
--- sinatra.rb
+++ sinatra.rb
@@ -4,8 +4,33 @@ require 'rack/test'
 set :port, 5500
 
 get '/' do
-  '<h1>Hello World</h1>
-   <a href="/llama">Click Here to see a Llama</a>'
+#   '<h1>Hello World</h1>
+#    <a href="/llama">Click Here to see a Llama</a>'
+    code = '<!DOCTYPE html>
+        <html lang="en" class="govuk-template ">
+        <head>
+            <title>Sinatra Example - GOV.UK</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+            <!--[if !IE 8]><!-->
+            <link rel="stylesheet" href="/stylesheets/govuk-frontend-4.3.1.min.css">
+            <!--<![endif]-->
+            <!--[if IE 8]>
+            <link rel="stylesheet" href="/stylesheets/govuk-frontend-ie8-4.3.1.min.css">
+            <![endif]-->
+        </head>
+        <body class="govuk-template__body ">
+            <script>"
+            document.body.className = ((document.body.className) ? document.body.className + " js-enabled" : "js-enabled");
+            </script>
+            <!-- component HTML -->
+            <script src="/javascript/govuk-frontend-4.3.1.min.js"></script>
+            <script>
+            window.GOVUKFrontend.initAll()
+            </script>
+        </body>
+    </html>'
+
+    erb code
 end
 
 get '/llama' do
