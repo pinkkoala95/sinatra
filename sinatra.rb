f6f7d75468f4929c09abd99015da04bb3337f45a
pinkkoala95 <emma.keeley@madetech.com>
2022-11-08 11:52:41 +0000

    updated styling on second page but image not showing

--- sinatra.rb
+++ sinatra.rb
@@ -5,13 +5,10 @@ set :port, 5500
 
 
 get '/' do
-#   '<h1>Hello World</h1>
-#    <a href="/llama">Click Here to see a Llama</a>'
     erb :home
 end
 
 get '/llama' do
-    '<h1><a href="/">Home</a></h1>
-    <img src=\'https://birdsofpreycentre.co.uk/wp-content/uploads/2021/05/PHOTO-2021-05-04-14-10-14-1.jpg\'/>'
+    erb :llama
 end
 
